2d6b4e5d788a2c9364f35574d862ce97fedb80cf
Chiken-nugget <71612911+Chiken-nugget@users.noreply.github.com>
2021-12-12 18:43:55 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,121 @@
+var canvas= new fabric.Canvas('myCanvas');
+block_image_width=30;
+block_image_height=30;
+player_x=200;
+player_y=200;
+var player_object="";
+function player_update(){
+    fabric.Image.fromURL("player.png",function(Img){
+        player_object=Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_y,
+            left:player_x,
+        })
+        canvas.add(player_object)
+    })
+}
+function new_image(get_image){
+    fabric.Image.fromURL(get_image,function(Img){
+        block_image_object=Img;
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set({
+            top:player_y,
+            left:player_x
+        })
+        canvas.add(block_image_object)
+    })
+}
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e){
+    keyPressed=e.keyCode;
+    console.log(keyPressed)
+    if(e.shiftkey==true && keyPressed==80){
+ console.log("P and shift pressed together");
+ block_image_width=block_image_width+10;
+ block_image_height=block_image_height+10;
+ document.getElementById("currentWidth").innerHTML=block_image_width;
+ document.getElementById("currentHeight").innerHTML=block_image_Height;
+    }
+ if(e.shiftkey==true && keyPressed==80){
+    console.log("M and shift pressed together");
+    block_image_width=block_image_width-10;
+    block_image_height=block_image_height-10;
+    document.getElementById("currentWidth").innerHTML=block_image_width;
+    document.getElementById("currentHeight").innerHTML=block_image_Height;
+ }
+ if(keyPressed=='70'){
+    new_image('assets/ironman_face.png');
+    console.log("f");
+}
+if(keyPressed=='66'){
+    new_image('assets/spiderman_body.png');
+    console.log("b");
+}
+if(keyPressed=='76'){
+    new_image('assets/hulk_legs.png');
+    console.log("l");
+}
+if(keyPressed=='82'){
+    new_image('assets/thor_right_hand.png');
+    console.log("r");
+}
+if(keyPressed=='72'){
+    new_image('assets/captain_america_left_hand.png');
+    console.log("h");
+}
+if(keyPressed=='38'){
+    up();
+    console.log("up");
+}
+if(keyPressed=='40'){
+    down();
+    console.log("down");
+}
+if(keyPressed=='37'){
+    left();
+    console.log("left");
+}
+if(keyPressed=='39'){
+    right();
+    console.log("right");
+}
+}
+function up(){
+    if(player_y>=0){
+        player_y=player_y-block_image_height;
+        console.log("Block image height="+block_image_height);
+        console.log("When up arrow key is pressed, X="+player_x+",Y="+player_y)
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function down(){
+    if(player_y<=500){
+        player_y=player_y+block_image_height;
+        console.log("Block image height="+block_image_height);
+        console.log("When down arrow key is pressed, X="+player_x+",Y="+player_y)
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function left(){
+    if(player_x>=0){
+        player_x=player_x-block_image_width;
+        console.log("Block image width="+block_image_width);
+        console.log("When left arrow key is pressed, X="+player_x+",Y="+player_y)
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function right(){
+    if(player_x<=850){
+        player_x=player_x+block_image_width;
+        console.log("Block image width="+block_image_width);
+        console.log("When right arrow key is pressed, X="+player_x+",Y="+player_y)
+        canvas.remove(player_object);
+        player_update();
+    }
+}
